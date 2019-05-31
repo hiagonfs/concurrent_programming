a0229316e83b7b2c11b0a92c88cca2e6b72eaefa
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2019-05-31 10:56:11 -0300

    novos testes

--- quest_02/src/quest_02/Main.java
+++ quest_02/src/quest_02/Main.java
@@ -6,38 +6,18 @@ public class Main {
 
 		final Channel canal01 = new ChannelImpl(2);
 
+		// producer
 		Thread th01 = new Thread(new Runnable() {
 
 			@Override
 			public void run() {
-				canal01.putMessage("um");
+				for (int i = 0; i < 5; i++) {
+					canal01.putMessage(Integer.toString(i + 1));
+				}
 			}
 		});
 
-		Thread th02 = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				canal01.putMessage("pequeno");
-			}
-		});
-
-		Thread th03 = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				canal01.putMessage("teste");
-			}
-		});
-
-		Thread th04 = new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				canal01.putMessage("fim");
-			}
-		});
-
+		//consumers
 		Thread th05 = new Thread(new Runnable() {
 
 			@Override
@@ -45,7 +25,7 @@ public class Main {
 				System.out.println(canal01.takeMessage());
 			}
 		});
-		
+
 		Thread th06 = new Thread(new Runnable() {
 
 			@Override
@@ -53,7 +33,7 @@ public class Main {
 				System.out.println(canal01.takeMessage());
 			}
 		});
-		
+
 		Thread th07 = new Thread(new Runnable() {
 
 			@Override
@@ -61,7 +41,7 @@ public class Main {
 				System.out.println(canal01.takeMessage());
 			}
 		});
-		
+
 		Thread th08 = new Thread(new Runnable() {
 
 			@Override
@@ -71,18 +51,12 @@ public class Main {
 		});
 
 		th01.start();
-		th02.start();
-		th03.start();
-		th04.start();
 		th05.start();
 		th06.start();
 		th07.start();
 		th08.start();
 
 		th01.join();
-		th02.join();
-		th03.join();
-		th04.join();
 		th05.join();
 		th06.join();
 		th07.join();
