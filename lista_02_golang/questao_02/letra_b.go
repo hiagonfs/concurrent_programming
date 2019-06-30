a974557d0096c1dffbd21c6bdcbc827c67c9dc9a
Hiago Fernandes <hiago.sousa@ccc.ufcg.edu.br>
2019-06-29 21:54:02 -0300

    add letra b

---
+++ lista_02_golang/questao_02/letra_b.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+"fmt"
+"time"
+"math/rand"
+"sync"
+)
+
+func sum_channel(ch chan int) int {
+	total := 0
+	for val := range ch {
+		total += val
+	}
+	return total
+}
+
+func request() int {	
+  rand.Seed(time.Now().UnixNano())
+  min := 1
+  max := 30
+  num := rand.Intn(max - min) + min
+  fmt.Println(num)
+  time.Sleep(time.Duration(num) * time.Second)
+  return num 
+}
+
+func gateway(num_replicas int) int {
+	 
+    messages := make(chan int) // canal de entrada de elementos
+    sum := make(chan int, num_replicas) // canal de saida da soma
+
+	go func() {
+		for i := 0; i < num_replicas; i++ {
+        	 messages <- request()
+		   	 
+        }
+		close(messages)
+    }()
+	
+	var wg sync.WaitGroup
+	wg.Add(num_replicas)
+	
+	for i := 0; i < num_replicas; i++ {
+		go func() {
+			sum <- sum_channel(messages)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	close(sum)
+	return sum_channel(sum)
+}
+
+func main() {
+
+  	chanResposta1 := make(chan int, 1)
+	chanResposta2 := time.Tick(16000 * time.Millisecond)
+	
+	chanResposta1 <-gateway(3) 
+
+	select {
+		
+		case result := <-chanResposta1:
+			fmt.Printf("Retorno %d", result) 
+			
+		case <-chanResposta2:
+			fmt.Println("-1")
+			 
+	}
+}
